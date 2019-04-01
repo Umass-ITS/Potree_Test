75030069732dda6e2a71661ba9f44df011394de0
Umass-ITS <48069308+Umass-ITS@users.noreply.github.com>
2019-04-01 01:26:30 -0400

    Testing data for potree on SR9

---
+++ T2_002/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 69940830,
+    "boundingBox": {
+        "lx": 376923.3955,
+        "ly": 2960177.47325,
+        "lz": 154.55025,
+        "ux": 384131.861,
+        "uy": 2967385.9387499999,
+        "uz": 7363.015749999991
+    },
+    "tightBoundingBox": {
+        "lx": 376923.3955,
+        "ly": 2960177.47325,
+        "lz": 154.55025,
+        "ux": 384131.861,
+        "uy": 2962669.72325,
+        "uz": 417.42875000000006
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "INTENSITY"
+    ],
+    "spacing": 62.42714309692383,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
